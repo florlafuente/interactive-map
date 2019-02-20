891b584600823e10e4d362f6f86791ce0f74be9e
Florencia Lafuente <lafuente.florencia@gmail.com>
2019-02-20 12:33:23 -0300

    Add Nav component

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,29 @@
 <template>
   <div id="app">
+    <Nav :activeTopic="activeTopic" :changeTopic="changeTopic"/>
     <Map />
   </div>
 </template>
 
 <script>
+import Nav from './components/Nav.vue'
 import Map from './components/Map.vue'
 
 export default {
   name: 'app',
   components: {
+    Nav,
     Map
+  },
+  data() {
+    return {
+      activeTopic: 'cupo-trans'
+    }
+  },
+  methods: {
+    changeTopic: function(newTopic) {
+      this.activeTopic = newTopic
+    }
   }
 }
 </script>
@@ -20,8 +33,6 @@ export default {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
---
+++ src/components/Nav.vue
@@ -0,0 +1,70 @@
+<template>
+  <nav class="nav-container">
+    <ul>
+      <li v-for="(topic, index) in topics" :key="index" :class="activeTopic === topic.value && 'active'">
+        <a @click="setNewTopic(topic.value)">
+        {{ topic.name }}
+        </a>
+      </li>
+    </ul>
+  </nav>
+</template>
+
+<script>
+  export default {
+    name: 'Nav',
+    props: {
+      activeTopic: {
+        type: String
+      },
+      changeTopic: {
+        type: Function
+      }
+    },
+    data() {
+      return {
+        topics: [
+          {
+            name: 'Cupo trans',
+            value: 'cupo-trans'
+          },
+          {
+            name: 'Aborto legal',
+            value: 'aborto-legal'
+          },
+          {
+            name: 'ESI',
+            value: 'esi'
+          }
+        ]
+      }
+    },
+    methods: {
+    setNewTopic: function(newTopic) {
+      return this.changeTopic(newTopic)
+    }
+  }
+  }
+</script>
+
+<style scoped>
+  nav {
+    margin-bottom: 40px;
+  }
+  ul {
+    display: flex;
+    justify-content: space-around;
+    list-style: none;
+  }
+  li {
+    cursor: pointer;
+    padding: 0 10px 10px;
+  }
+  li:hover {
+    font-weight: bold;
+  }
+  li.active {
+    font-weight: bold;
+    border-bottom: 2px solid #f38eb0;
+  }
+</style>
